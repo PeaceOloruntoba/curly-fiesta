aca8b8b33204308adec0f10dafa7fb9eb7e5c58a
PeaceOloruntoba <profprincepeace@gmail.com>
2025-11-06 03:17:05 +0100

    d

--- src/middlewares/rateLimit.ts
+++ src/middlewares/rateLimit.ts
@@ -1,4 +1,6 @@
-import rateLimit from 'express-rate-limit';
+import rateLimitPkg from 'express-rate-limit';
+
+const rateLimit = (rateLimitPkg as any).default ?? (rateLimitPkg as any);
 
 export const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
