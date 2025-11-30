7e0daa4494dc955f047d06dbeeca6ca3166b9250
PeaceOloruntoba <profprincepeace@gmail.com>
2025-11-30 17:21:38 +0100

    okayyy

--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { requireAuth } from '../../middlewares/auth.js';
-import { enforceSubscription } from '../../middlewares/subscription.js';
 import * as ctrl from '../../controllers/usersController.js';
 
 const router = Router();
 
-router.get('/me', requireAuth, enforceSubscription, ctrl.me);
+router.get('/me', requireAuth, ctrl.me);
 
 export default router;
